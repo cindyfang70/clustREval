0977c150fa9c68196893b5ab9c9f0c880d74bb34
Cindy Fang <cindyfang70@gmail.com>
2021-11-10 17:14:13 -0500

    working implementation of runPipelineCombs

--- R/runPipelineCombs.R
+++ R/runPipelineCombs.R
@@ -1,4 +1,5 @@
 library(pipeComp)
+library(scuttle)
 
 
 #' runPipelineCombs
@@ -19,7 +20,7 @@ library(pipeComp)
 #' @return end outputs object
 #' @export
 
-runPipelineCombs <- function(sce, doubletmethod =c("none"), filt=c("default"), norm=c("norm.seurat"), sel=c("sel.vst"), selnb=2000, dr=c("seurat.pca"), clustmethod=c("clust.seurat"), dims=c(10), resolution=c(0.1, 0.2)){
+runPipelineCombs <- function(sce, doubletmethod =c("none"), filt=c("filt.default"), norm=c("norm.seurat"), sel=c("sel.vst"), selnb=2000, dr=c("seurat.pca"), clustmethod=c("clust.seurat"), dims=c(10), resolution=c(0.1, 0.2)){
   alternatives <- list(
     doubletmethod=doubletmethod,
     filt=filt,
@@ -32,6 +33,40 @@ runPipelineCombs <- function(sce, doubletmethod =c("none"), filt=c("default"), n
     resolution=resolution
   )
   source(system.file("extdata", "scrna_alternatives.R", package="pipeComp"))
+  
+  sce <- scuttle::addPerCellQC(sce)
+  sce <- scuttle::addPerFeatureQC(sce)
+  sce <- add_meta(sce)
+
+  pct_counts_in_top_50_features <- sce$pct_counts_top_50_features
+  sce$pct_counts_in_top_50_features <- pct_counts_in_top_50_features
+  sce$pct_counts_top_50_features <- NULL
+  sce$pct_counts_Mt <- sce$pct_Mt
+  sce$pct_Mt <- NULL
+  sce$pct_counts_in_top_20_features <- sce$percent.top_20
+  sce$percent.top_20 <- NULL
+  
+  sce$phenoid <- rep(0, ncol(sce))
+
+  sce <- c(sce=sce)
   pip_def <- pipeComp::scrna_pipeline(pipeClass = "seurat")
-  res <- pipeComp::runPipeline(sce, alternatives, pip_def, nthreads=1)
+  evalDummy <- function(...){
+    return(1)
+  }
+  pip_def@evaluation$doublet <- evalDummy
+  pip_def@evaluation$filtering <- evalDummy
+  pip_def@evaluation$normalization <- evalDummy
+  pip_def@evaluation$selection <- evalDummy
+  pip_def@evaluation$dimreduction <- evalDummy
+  pip_def@evaluation$clustering <- evalDummy
+  
+  
+  pip_def@aggregation$doublet <- evalDummy
+  pip_def@aggregation$filtering <- evalDummy
+  pip_def@aggregation$normalization <- evalDummy
+  pip_def@aggregation$selection <- evalDummy
+  pip_def@aggregation$dimreduction <- evalDummy
+  pip_def@aggregation$clustering <- evalDummy
+  
+  res <- pipeComp::runPipeline(sce, alternatives, pip_def, nthreads=1, debug=TRUE)
 }
