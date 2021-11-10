f57641e290a0730dfe994302b18f20db888e62f3
Cindy Fang <cindyfang70@gmail.com>
2021-11-10 16:02:02 -0500

    Add embryo scRNA-seq dataset

---
+++ R/data.R
@@ -0,0 +1,8 @@
+#' Tracing pluripotency of human early embryos and embryonic stem cells by single cell RNA-seq
+#' 
+#'
+#' Gene expression of 124 early embryo and embryonic stem cells from scRNA-seq. Sourced from the
+#' EMBL-EBI Single Cell Expression Atlas.
+#' @format A data frame with 26073 rows and 124 columns:
+#' @source \url{https://www.ebi.ac.uk/gxa/sc/experiments/E-GEOD-36552/results/tsne}
+"embryo"
--- R/runPipelineCombs.R
+++ R/runPipelineCombs.R
@@ -4,19 +4,31 @@ library(pipeComp)
 #' runPipelineCombs
 #'
 #'Run user-defined clustering pipeline combinations on a user-specificed scRNA-seq dataset.
+#'
 #' @param sce SingleCellExperiment object with counts matrix and phenoid colData entry.
 #' @param filt list of filtering methods
 #' @param norm list of normalization methods
 #' @param resolution list of clustering resolutions
+#' @param doubletmethod list of doublet detection methods
+#' @param sel list of selection methods
+#' @param selnb list of number of genes to select
+#' @param dr list of dimensionality reduction methods
+#' @param clustmethod list of clustering methods
+#' @param dims list of dimensions for dimensionality reduction
 #'
 #' @return end outputs object
 #' @export
 
-runPipelineCombs <- function(sce, filt, norm, resolution){
-  
+runPipelineCombs <- function(sce, doubletmethod =c("none"), filt=c("default"), norm=c("norm.seurat"), sel=c("sel.vst"), selnb=2000, dr=c("seurat.pca"), clustmethod=c("clust.seurat"), dims=c(10), resolution=c(0.1, 0.2)){
   alternatives <- list(
+    doubletmethod=doubletmethod,
     filt=filt,
     norm=norm,
+    sel=sel,
+    selnb=selnb, 
+    dr=dr,
+    clustmethod=clustmethod,
+    dims=dims,
     resolution=resolution
   )
   source(system.file("extdata", "scrna_alternatives.R", package="pipeComp"))
