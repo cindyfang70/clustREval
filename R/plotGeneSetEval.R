e82e57880872d8e59276ddcdf08edf3de583464c
Cindy Fang <cindyfang70@gmail.com>
2021-11-16 21:32:18 -0500

    remove require()

--- R/plotGeneSetEval.R
+++ R/plotGeneSetEval.R
@@ -24,19 +24,17 @@ library(ggplot2)
 
 plotGeneSetEval <- function(gseas){
   plots <- c()
-  suppressPackageStartupMessages(require(ggplot2))
   for(i in 1:length(gseas)){
     gseas[[i]]$pathway <- gsub("HALLMARK_", "", gseas[[i]]$pathway)
-    p <- ggplot2::ggplot(gseas[[i]], aes(reorder(pathway, NES), NES)) +
+    p <- ggplot2::ggplot(gseas[[i]], ggplot2::aes(reorder(pathway, NES), NES)) +
       ggplot2::geom_col(aes(fill=padj<0.05)) +
       ggplot2::coord_flip() +
-      labs(x="Pathway", y="Normalized Enrichment Score",
+      ggplot2::labs(x="Pathway", y="Normalized Enrichment Score",
         title=sprintf("Hallmark pathways NES from GSEA for Cluster %s", names(gseas)[[i]]))+
-      theme(axis.text.y=element_text(size=3))
+      ggplot2::theme(axis.text.y=element_text(size=3))
     plots <- c(plots, list(p))
   }
   n <- length(plots)
   nCol <- floor(sqrt(n))
-  suppressPackageStartupMessages(require(gridExtra))
-  do.call("grid.arrange", c(plots=plots, ncol=nCol))
+  do.call("gridExtra::grid.arrange", c(plots=plots, ncol=nCol))
 }
